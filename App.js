cedae376eb47d440d33e06551a22f3d857cc472a
Hernan <hernan@mukiku.com>
2022-01-13 11:41:16 -0300

    'init_and_install_react-navigation'

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" />
+    </NavigationContainer>
   );
 }
 
