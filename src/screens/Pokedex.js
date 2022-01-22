efbf71d1afb85dab1f4ce2bc3125da65fd67cfad
Hernan <hernan@mukiku.com>
2022-01-22 01:28:47 -0300

    'API-get_array_pokemons'

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet, SafeAreaView, Text, Platform, StatusBar } from 'react-native'
+import {getPokemonApi} from '../api/pokemon'
 
 export default function Pokedex() {
+
+    useEffect(() => {
+        // funcion anonima autoejecutable
+        (async() => {
+            await loadPokemons()
+        })()
+            }, [])
+
+    const loadPokemons = async () => {
+        try {
+            const response = await getPokemonApi()
+            console.log(response)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+
     return (
         <SafeAreaView style={styles.AndroidSafeArea}>
             <Text>Pokedex Screen</Text>
